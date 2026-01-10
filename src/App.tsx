9ca8a9e5d1d576c3a67979bf646218634ecb0576
Doniyor Latipov <doniyor.main.contact@gmail.com>
2026-01-10 23:47:28 +0300

    feat: add App and Index entry points

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+import Column from './components/Column/Column';
+import Card from './components/Card/Card';
+
+function App() {
+  return (
+    <div className="kanban">
+      <Column title="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+
+      <Column title="In progress">
+        <Card text="Learn TypeScript" />
+      </Column>
+
+      <Column title="Done">
+        <Card text="Begin to use static typing"></Card>
+      </Column>
+    </div>
+  );
+}
+
+export default App;
