6b636a19764fa4bd4e3f73d1cdbb560c3415f812
DoniyorLatipov <doniyor.main.contact@gmail.com>
2026-01-17 12:07:56 +0300

    styles(Column): update component structure and styles according to design system

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -8,7 +8,9 @@ interface ColumnProps {
 export default function Column({ title, children }: React.PropsWithChildren<ColumnProps>) {
   return (
     <div className={classes.column}>
-      <h2 className={classes.columnTitle}>{title}</h2>
+      <div className={classes.columnHeader}>
+        <h2 className={classes.columnTitle}>{title}</h2>
+      </div>
       <div className={classes.columnContainer}>{children}</div>
     </div>
   );
