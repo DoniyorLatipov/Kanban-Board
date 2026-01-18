9ad578d2a880e89af7ee2989e51b3878835ff487
DoniyorLatipov <doniyor.main.contact@gmail.com>
2026-01-18 05:20:08 +0300

    feat(hook): add hook for interacting with globalAppState

---
+++ src/hooks/useAppState.ts
@@ -0,0 +1,6 @@
+import React, { useContext } from 'react';
+import { AppStateContext } from '../contexts/AppStateContext';
+
+export function useAppState() {
+  return useContext(AppStateContext);
+}
