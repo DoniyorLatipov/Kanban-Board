ab5a0f24b84a534c542dd540fb53bed64f1d7cd0
DoniyorLatipov <doniyor.main.contact@gmail.com>
2026-01-20 06:20:47 +0300

    feat(lists): add form for creating lists and use uuid for id generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import './styles/App.css';
 import Column from './components/Column/Column';
+import AddNewItem from './components/AddNewItem/AddNewItem';
 import { useAppState } from './hooks/useAppState';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const { state } = useAppState();
+  const { state, dispatch } = useAppState();
 
   return (
     <div className="kanban">
       {state.lists.map((list) => {
         return <Column title={list.title} key={list.id} id={list.id} />;
       })}
+
+      <AddNewItem
+        toogleButtonText="+ Add another list"
+        onAddItem={(text) => dispatch({ type: 'ADD_LIST', payload: { title: text, id: uuidv4() } })}
+        theme="primary"
+      />
     </div>
   );
 }
