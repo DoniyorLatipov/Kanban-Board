89af1557639f76ad80ff76feb19bfe72b107b9f8
DoniyorLatipov <doniyor.main.contact@gmail.com>
2026-01-20 06:24:31 +0300

    feat(tasks): add form for creating tasks, use uuid for id, and update id type

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import classes from './Column.module.css';
+import AddNewItem from '../AddNewItem/AddNewItem';
 import Card from '../Card/Card';
 import { useAppState } from '../../hooks/useAppState';
+import { v4 as uuidv4 } from 'uuid';
 
 interface ColumnProps {
   title: string;
-  id: number;
+  id: string;
 }
 
 export default function Column({ title, id }: ColumnProps) {
-  const { state } = useAppState();
+  const { state, dispatch } = useAppState();
 
   return (
     <div className={classes.column}>
@@ -22,6 +24,15 @@ export default function Column({ title, id }: ColumnProps) {
           .map(({ text, id }) => (
             <Card text={text} key={id} />
           ))}
+      </div>
+      <div className={classes.columnFooter}>
+        <AddNewItem
+          toogleButtonText="+ Add another card"
+          onAddItem={(text) =>
+            dispatch({ type: 'ADD_TASK', payload: { text, id: uuidv4(), listId: id } })
+          }
+          theme="light"
+        />
       </div>
     </div>
   );
