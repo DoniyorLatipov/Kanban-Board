fd79efc64e6c358554c6cb170a24438279dd92a0
DoniyorLatipov <doniyor.main.contact@gmail.com>
2026-01-25 02:24:43 +0300

    feat(column): add drag and drop functionality

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -4,6 +4,8 @@ import AddNewItem from '../AddNewItem/AddNewItem';
 import Card from '../Card/Card';
 import { useAppState } from '../../hooks/useAppState';
 import { v4 as uuidv4 } from 'uuid';
+import { useDragItem } from '../../hooks/useDragItem';
+import { useDropItem } from '../../hooks/useDropItem';
 
 interface ColumnProps {
   title: string;
@@ -13,8 +15,12 @@ interface ColumnProps {
 export default function Column({ title, id }: ColumnProps) {
   const { state, dispatch } = useAppState();
 
+  // need rewrite as well as dragRef(dropRef(node)) on 23
+  const { dragRef } = useDragItem({ id, type: 'COLUMN' });
+  const { dropRef } = useDropItem({ id, type: 'COLUMN' });
+
   return (
-    <div className={classes.column}>
+    <div className={classes.column} ref={(node) => void dragRef(dropRef(node))}>
       <div className={classes.columnHeader}>
         <h2 className={classes.columnTitle}>{title}</h2>
       </div>
